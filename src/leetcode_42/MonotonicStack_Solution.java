32bcefa64bbe9063187a2c34df0423908ea97426
LRX <461834084@qq.com>
2022-05-26 15:54:15 +0800

    力扣42题接雨水单调栈方法官方解答
    LRX

--- src/leetcode_42/MonotonicStack_Solution.java
+++ src/leetcode_42/MonotonicStack_Solution.java
@@ -1,5 +1,6 @@
 package leetcode_42;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,6 +24,32 @@ public class MonotonicStack_Solution {
                 }
                 ((LinkedList<Integer>) stack).push(i);
             }
+        }
+        return ans;
+    }
+
+    /**
+     * 官方写法的单调栈方法
+     *
+     * @param height
+     * @return
+     */
+    public int OfficalAns_trap(int[] height) {
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int n = height.length;
+        for (int i = 0; i < n; ++i) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int left = stack.peek();
+                int currWidth = i - left - 1;
+                int currHeight = Math.min(height[left], height[i]) - height[top];
+                ans += currWidth * currHeight;
+            }
+            stack.push(i);
         }
         return ans;
     }
